a60e371a07ae3f4c017620a9094fde61f81b9907
Kizer <chuks.onuegbu@gmail.com>
2013-06-13 01:30:00 +0100

    Removed email files saving

--- www/smtp/server.js
+++ www/smtp/server.js
@@ -27,12 +27,12 @@ smtp.on("startData", function(connection){
 });
 
 smtp.on("data", function(connection, chunk){
-    connection.saveStream.write(chunk);
+    //connection.saveStream.write(chunk);
     connection.data += chunk;
 });
 
 smtp.on("dataReady", function(connection, callback){
-    connection.saveStream.end();
+    //connection.saveStream.end();
     console.log("Incoming message saved to %s", connection.tmpFile);
     callback(null, "ABC1"); // ABC1 is the queue id to be advertised to the client
     // callback(new Error("Rejected as spam!")); // reported back to the client
